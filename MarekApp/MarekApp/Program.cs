2172abd9a1811aa3aa51e966609b24ee9610ee59
Marekbmw36 <agaim1@wp.pl>
2023-07-23 14:54:09 +0200

    zadanie domowe nr4

---
+++ MarekApp/MarekApp/Program.cs
@@ -0,0 +1,18 @@
+﻿string name = "Marek";
+var age = 27;
+bool woman = true;
+
+if (woman == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa,lat 33");
+}
+else if (woman == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+        
+
