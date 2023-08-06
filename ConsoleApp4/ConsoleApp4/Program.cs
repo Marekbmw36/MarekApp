563b22822fc896f89663c96f66552c2769690908
Marekbmw36 <agaim1@wp.pl>
2023-08-06 22:00:42 +0200

    praca domowa nr 6

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using ConsoleApp4;
+
+Employee employee1 = new Employee("Piotr", "Nowak", 27);
+Employee employee2 = new Employee("Michał", "Kowalski", 33);
+Employee employee3 = new Employee("Łukasz", "Zdanewicz", 24);
+
+employee1.Addscore(5);
+employee1.Addscore(4);
+employee1.Addscore(1);
+employee1.Addscore(6);
+employee1.Addscore(5);
+
+employee2.Addscore(5);
+employee2.Addscore(7);
+employee2.Addscore(5);
+employee2.Addscore(9);
+employee2.Addscore(3);
+
+employee3.Addscore(5);
+employee3.Addscore(3);
+employee3.Addscore(2);
+employee3.Addscore(5);
+employee3.Addscore(4);
+
+
+List<Employee> employees = new List<Employee>()
+{
+   employee1 ,employee2 , employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        employeeWithMaxResult = employee;
+        maxResult = employeeWithMaxResult.Result;
+    }
+
+}
+Console.WriteLine("Pracownik " + employeeWithMaxResult.Name + " " + "" + employeeWithMaxResult.Surname + " " + "" + (employeeWithMaxResult.Age + "lat: ") + "" + "uzyskał największą liczbę ocen " + "" + employeeWithMaxResult.Result);
+
+
+
+
+
+
+
+
